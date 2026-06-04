38fc8b84d42b628d26e6ef2233753468a57488dd
Matias Rueda <134736309+matiaschancleta@users.noreply.github.com>
2026-06-04 12:50:40 -0300

    Cambios de organización

--- Assets/Scripts/Level/LevelStarter.cs
+++ Assets/Scripts/Level/LevelStarter.cs
@@ -59,11 +59,11 @@ public class LevelStarter : MonoBehaviour, IInteractable
                 {
                     if (mat.HasProperty("_Color"))
                     {
-                        mat.SetColor("_Color", Color.black);
+                        mat.SetColor("_Color", new Color32(1, 1, 1, 255));
                     }
                 }
             }
-            RenderSettings.ambientLight = Color.black;
+            RenderSettings.ambientLight = new Color32(1, 1, 1, 255);
         }
     }
 }
